2ee36de1d318fdd2114cbda0211509342f358da7
KicaRonaldOkello <okello.ronald045@gmail.com>
2018-10-03 11:41:12 +0300

    [Feature #160949686] Sign in method and password

--- app/signin.py
+++ app/signin.py
@@ -1,4 +1,4 @@
-
+from datetime import datetime
 class SignIn:
     User = [{"username":"kica", "password":"12345","role":"bootcamp"}]
     
@@ -6,7 +6,9 @@ class SignIn:
     def signin(self, username, password):
         for k in self.User:
             if k["username"] == username and k["password"] == password:
+                k.update({"login_time":str(datetime.now())})
                 return k
         return None
-
+a = SignIn()
+print(a.signin("kica","12345"))
 
